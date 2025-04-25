2651eca98c1c3cd78ce7cf3a5bdedfe1f485608e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-25 12:10:16 +0600

    added new file

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,3 +1,5 @@
+import About from '../About/About';
+import Cart from '../Cart/Cart';
 import './CartContainer.css';
 const CartContainer = ({ handleIsActiveState, isActive }) => {
     // console.log(isActive)
@@ -10,6 +12,12 @@ const CartContainer = ({ handleIsActiveState, isActive }) => {
                 <div onClick={() => handleIsActiveState("cart")} className={`${isActive.cart ? "btn active" : "btn"}`}>Cart</div>
                 <div onClick={() => handleIsActiveState("about")} className={`${isActive.cart ? "btn" : "btn active"}`}>About</div>
             </div>
+            {/* another conditional rendering */}
+            {
+                isActive.cart ? <Cart></Cart> : <About></About>
+            }
+
+
         </div>
     );
 };
